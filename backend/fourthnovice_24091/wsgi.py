11184f054d345e7b08cabaef967538e80f8ab546
Crowdbotics <team@crowdbotics.com>
2021-01-26 15:56:45 +0000

    Update models

--- backend/fourthnovice_24091/wsgi.py
+++ backend/fourthnovice_24091/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fourthnovice_24091.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fourthnovice_24091.settings")
 
 application = get_wsgi_application()
